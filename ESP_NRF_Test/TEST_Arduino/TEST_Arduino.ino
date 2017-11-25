568edb5d21aee463f7ded2d02cc2187614b1a85c
Unknown <dozedler@gmail.com>
2017-11-25 14:36:58 +0100

    example code

---
+++ ESP_NRF_Test/TEST_Arduino/TEST_Arduino.ino
@@ -0,0 +1,29 @@
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define button 4
+
+RF24 radio(7,8); // CNS, CE
+
+boolean buttonState = 0;
+
+const byte address[6] = "00001";
+
+void setup() {
+  pinMode(button, INPUT);
+  Serial.begin(9600);
+  Serial.print("starting up radio");
+  radio.begin();
+  Serial.println("...done");
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+}
+
+void loop() {
+    buttonState = digitalRead(button);
+    radio.write(&buttonState, sizeof(buttonState));
+    delay(5);
+}
---
+++ ESP_NRF_Test/TEXT_ESP/TEXT_ESP.ino
@@ -0,0 +1,41 @@
+
+#include <ESP8266WiFi.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define led D1
+
+
+
+RF24 radio(D4, D8); // CNS, CE
+
+const byte address[6] = "00001";
+boolean buttonState = 0;
+
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.startListening();
+  Serial.println("hello");
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+
+  radio.startListening();
+  while (!radio.available());
+  radio.read(&buttonState, sizeof(buttonState));
+  if (buttonState == HIGH) {
+    digitalWrite(led, HIGH);
+    Serial.println("HIGH");
+  }
+  else {
+    digitalWrite(led, LOW);
+    Serial.println("LOW");
+  }
+}
